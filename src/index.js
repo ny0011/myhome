1167342331e5d9f4bef46deea632deb07ecf6674
ny0011 <nayun011@gmail.com>
2022-04-11 14:44:15 +0900

    Use createRoot instead ReactDom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { Reset } from "styled-reset";
 import App from "./App";
-ReactDOM.render(
+
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
+    <Reset />
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
