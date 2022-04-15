e07c77637312214e2ac059ef0da4b50ea1dea8e5
ny0011 <nayun011@gmail.com>
2022-04-15 17:51:19 +0900

    refactor bookmark

--- src/Routes/Bookmark.tsx
+++ src/Routes/Bookmark.tsx
@@ -1,45 +1,7 @@
-import { useState } from "react";
-import { MotionButton } from "../Styles/Motions";
-import styled from "styled-components";
-import Div from "../Styles/Tags";
-
-const BookmarkDiv = styled(Div)`
-  position: relative;
-`;
-
-const BookmarkToggleButton = styled(MotionButton)`
-  position: absolute;
-`;
-
-const variants = {
-  open: { opacity: 1, scale: 1 },
-  closed: { opacity: 0, scale: 0 },
-};
+import BookmarkButton from "../Components/BookmarkButton";
 
 function Bookmark() {
-  const [isOpen, setIsOpen] = useState(true);
-  const toggleButton = () => setIsOpen((isOpen) => !isOpen);
-
-  return (
-    <BookmarkDiv>
-      <BookmarkToggleButton
-        initial="open"
-        variants={variants}
-        onClick={toggleButton}
-        animate={isOpen ? "open" : "closed"}
-      >
-        🧡
-      </BookmarkToggleButton>
-      <BookmarkToggleButton
-        initial="close"
-        onClick={toggleButton}
-        animate={isOpen ? "closed" : "open"}
-        variants={variants}
-      >
-        🤍
-      </BookmarkToggleButton>
-    </BookmarkDiv>
-  );
+  return <BookmarkButton />;
 }
 
 export default Bookmark;
