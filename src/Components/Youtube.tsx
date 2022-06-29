26627717625af7dc31ec51cd2eb5f7d82c0e0438
ny0011 <nayun011@gmail.com>
2022-06-29 23:24:53 +0900

    get latest video
    - 유튜버의 최신 영상 링크 연결

--- src/Components/Youtube.tsx
+++ src/Components/Youtube.tsx
@@ -1,14 +1,23 @@
 import { useQuery } from "react-query";
-import { IYoutuber, getYoutubers } from "../api";
+import { getNewVideos, INewVideo, getUploadLink } from "../api";
 
 interface Iprops {
   link: string;
 }
 
 function Youtube({ link }: Iprops) {
-  const { data, isLoading } = useQuery<any>(["youtuber", link]);
-  console.log(getYoutubers(link));
-  return <div>{isLoading ? <div>...</div> : <div>YAY</div>}</div>;
+  const { data, isLoading } = useQuery<INewVideo>(
+    ["youtuber", link],
+    async () => {
+      const uploadLink = await getUploadLink(link);
+      return await getNewVideos(uploadLink);
+    },
+    { refetchOnWindowFocus: false }
+  );
+  console.log(data);
+  const url = `https://www.youtube.com/watch?v=${data?.videoId}`;
+
+  return <div>{isLoading ? <div>...</div> : <a href={url}>YAY</a>}</div>;
 }
 
 export default Youtube;
--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,7 @@
 const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
-const CHANNEL_API_LINK = "https://www.googleapis.com/youtube/v3/channels";
+const YOUTUBE_LINK = "https://www.googleapis.com/youtube/v3";
+const CHANNEL_API_LINK = `${YOUTUBE_LINK}/channels`;
+const PLAYLISTITEM_API_LINK = `${YOUTUBE_LINK}/playlistItems`;
 
 interface IThumbnails {
   default: {
@@ -8,10 +10,24 @@ interface IThumbnails {
     width: number;
   };
 }
-
 export interface IYoutuber {
   thumbnails: IThumbnails;
   title: string;
+}
+export interface IChannelsList {
+  kind: string;
+  etag: string;
+  nextPageToken: string;
+  prevPageToken: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: any[];
+}
+export interface INewVideo {
+  videoId: string;
+  videoPublishedAt: string;
 }
 
 export const getYoutubers = (link: string) => {
@@ -27,5 +43,28 @@ export const getYoutubers = (link: string) => {
         thumbnails: { default: t },
       };
       return youtuber;
+    });
+};
+
+export const getUploadLink = (link: string) => {
+  return fetch(
+    `${CHANNEL_API_LINK}/?part=contentDetails&id=${link}&key=${YOUTUBE_API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      const { uploads } = response.items[0].contentDetails.relatedPlaylists;
+      return uploads;
+    });
+};
+
+export const getNewVideos = (link: string) => {
+  return fetch(
+    `${PLAYLISTITEM_API_LINK}/?part=contentDetails&playlistId=${link}&key=${YOUTUBE_API_KEY}&maxResults=1`
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      const { videoId, videoPublishedAt } = response.items[0].contentDetails;
+
+      return { videoId, videoPublishedAt };
     });
 };
