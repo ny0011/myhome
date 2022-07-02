0722467b76d786f12b747772a5de6577317f5644
ny0011 <nayun011@gmail.com>
2022-07-03 02:19:50 +0900

    view thumbnail when width is big

--- src/Components/BookmarkList.tsx
+++ src/Components/BookmarkList.tsx
@@ -2,14 +2,43 @@ import { useRecoilValue } from "recoil";
 import { bookmarkState, newVideoState } from "../atoms";
 import BookmarkItem from "./BookmarkItem";
 import { BookmarkListContainer } from "../Styles/BookmarkUI";
+import { theme } from "../theme";
+import { useEffect, useState } from "react";
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
 
 function BookmarkList() {
   const bookmarks = useRecoilValue(bookmarkState);
   const newvideo = useRecoilValue(newVideoState);
+  const { height, width } = useWindowDimensions();
+  const tablet = parseInt(theme.device.tablet.split(" ")[3].split("px")[0]);
 
   return (
     <BookmarkListContainer>
-      {newvideo.title ? (
+      {newvideo.title && width > tablet ? (
         <img
           src={newvideo.thumbnail.url}
           height={newvideo.thumbnail.height}
